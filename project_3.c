ca62a606d390472a50e84b542c90207c04f37e33
Astoner <aidanstoner1@gmail.com>
2023-10-09 16:23:45 -0400

    Obj 2 Done
    Objective 2 is done and working cange the num1 variable to display different values

--- project_3.c
+++ project_3.c
@@ -2,6 +2,29 @@
 //Date: 10/16/13
 //Course: ELEC-3371
 //Description: lets try this again
+//******************************************************************************
+//Global Variables:
+int num1 = 0xA500; //Variables for 7-seg display
+int prevPA0; //holds previous value of PA0 for falling/rising edge purposes
+//******************************************************************************
+//Main Function
 void main() {
+//Initializations
+     RCC_APB2ENR |= 1 << 2;  //enables clock for PortA
+     RCC_APB2ENR |= 1 << 5;  //enables clock for PortD
+     RCC_APB2ENR |= 1 << 6;  //enables clock for PortD    need to choose output port and enable it
+     
+     GPIOA_CRL = 0x44444444; //Set PortA as an input
+     GPIOA_CRH = 0x44444444;
+     GPIOD_CRL = 0x44444444; //Set PortD as an input
+     GPIOD_CRH = 0x44444444;
+     GPIOE_CRL = 0x33333333; //Set PortE as an output for LEDS
+     GPIOE_CRH = 0x33333333;
+     while(1){
+//Objective 2
+          GPIOE_ODR = num1;   //PE13 and PE15 need to be high in order for the 7-segments to display a number. PE11 switch which
+                              //7-segment is being displayed, PE8, PE10, PE12, & PE14 are data pins which will display the
+                              //decimal version of their binary value 0-9.
 
+     }
 }
